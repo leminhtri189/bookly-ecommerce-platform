025005504dfb8666390c7ab63ff20fb2f500b9bc
Lê Minh Trí <trilm189@gmail.com>
2026-01-03 19:31:23 +0700

    [CREATE] add UI for basic layout

---
+++ BooklyShop/BooklyShop.Presentation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using BooklyShop.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooklyShop.Presentation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BooklyShop/BooklyShop.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BooklyShop.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
