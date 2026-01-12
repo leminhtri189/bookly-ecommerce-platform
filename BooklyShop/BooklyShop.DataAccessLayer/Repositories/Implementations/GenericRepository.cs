cca983ca8454f30e15581742027c6db0030d574e
Lê Minh Trí <trilm189@gmail.com>
2026-01-12 19:04:49 +0700

    [UPDATE] update structor for project

---
+++ BooklyShop/BooklyShop.DataAccessLayer/Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using BooklyShop.BusinessObject.Model;
+using BooklyShop.DataAccessLayer.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BooklyShop.DataAccessLayer.Repositories.Implementations
+{
+    public class GenericRepository
+    {
+       
+    }
+}
